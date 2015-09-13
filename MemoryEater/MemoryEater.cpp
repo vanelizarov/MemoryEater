051196c4198198a082cd9898f8b788d3b6d19399
vanelizarov <elizarov.vanya@gmail.com>
2015-09-14 00:59:32 +0300

    Initial commit

---
+++ MemoryEater/MemoryEater.cpp
@@ -0,0 +1,24 @@
+// MemoryEater.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	for (;;) {
+
+		for (;;) {
+
+			new double;
+
+		}
+		
+	}
+
+	system("pause");
+	return 0;
+}
+
